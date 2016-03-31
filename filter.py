30d9dc451caf38a15ddcebaac7a76cba663455b5
susanjiang03 <susanjiang03@gmail.com>
2016-03-31 01:08:05 -0400

    example line chartfor location 3000

--- filter.py
+++ filter.py
@@ -7,11 +7,16 @@ datafile = "indicators.csv"
 #filter data on one location by index for all dates, land-mask
 def filter_by_index(in_filename, index):
     with open(in_filename, 'rb') as in_f:
+
         datareader = csv.reader(in_f)
+        header = datareader.next()
         next(datareader, None) #skip the headers
+
         out_filename = "location_"+str(index)+".csv"
         out_f = open(out_filename,'wb')
         writer = csv.writer(out_f)
+        writer.writerow(header)
+
         print "Start filtering data for location : "+str(index)
         start = datetime.datetime.now()
 
@@ -28,21 +33,28 @@ def filter_by_index(in_filename, index):
 #filter data on one date for all locations
 def filter_by_date(in_filename, date):
     with open(in_filename, 'rb') as in_f:
+
         datareader = csv.reader(in_f)
+        header = datareader.next()
         next(datareader, None) #skip the headers
+
         out_filename = "date_"+date+".csv"
         out_f = open(out_filename,'wb')
         writer = csv.writer(out_f)
+        writer.writerow(header)
+
         print "Start filtering data for date :"+date
         start = datetime.datetime.now()
 
         for line in datareader:
             if line[1] == date:
                 writer.writerow(line)
+
         end = datetime.datetime.now()
         elapsedTime = end - start
         print "Finished filter_by_date, elapsedTime : " + str(elapsedTime)
 
 
-#filter_by_index(datafile, 0)
+
+#filter_by_index(datafile, 3000)
 #filter_by_date(datafile, "2005-12-01")
