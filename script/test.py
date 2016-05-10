dbf49c9dd4769a327c982d5d88600798f13a5b93
susanjiang03 <susanjiang03@gmail.com>
2016-05-10 08:41:11 -0400

    add filtered data csv files, and sample files, compressed

--- script/test.py
+++ script/test.py
@@ -1,6 +1,11 @@
 
 import os
 
+# import matplotlib.pyplot as plt
+# plt.hist([1, 2, 1], bins=[0, 1, 2, 3])
+# #(array([0, 2, 1]), array([0, 1, 2, 3]), <a list of 3 Patch objects>)
+# plt.show()
+
 CURRENT_DIR = os.getcwd()
 PARENT_DIR = CURRENT_DIR[:CURRENT_DIR.rfind("/")]
 DateCSV_DIR = PARENT_DIR + "/DateCSV"
@@ -9,11 +14,12 @@ SampleCSV_DIR = PARENT_DIR + "/SampleCSV"
 SampleLocationsTXT_DIR = PARENT_DIR + "/SampleLocationsTXT"
 
 
-old = "_sample_"
-new =  "_"
-the_dir = SampleCSV_DIR + "/P0.05_N1"
+old = "archive"
+new =  "SampleCSVarchive"
+the_dir = PARENT_DIR
+print PARENT_DIR
 for fileName in os.listdir(the_dir):
-    if old in fileName:
+    if fileName.startswith(old):
         newName = fileName.replace(old, new)
         os.rename(os.path.join(the_dir,fileName), os.path.join(the_dir,newName))
         print "rename in %s\n%s"%(the_dir,newName) 
