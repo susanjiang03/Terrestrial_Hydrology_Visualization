88591274255abc39291374a0dbff43b92d3ec70f
Tarekul Islam <bayridge4th@gmail.com>
2016-05-16 19:54:08 -0400

    d3 map

---
+++ script/d3/test.js
@@ -0,0 +1,17 @@
+var test_json = {
+	"features":[ 
+	{
+          
+            "id": 1,
+            "type": "Point",
+            "coordinates" :[ -155.79903, 19.6795] 
+            
+    },
+    {
+    	
+    	"id": 41, 
+        "start_date": "1991-12-01", 
+        "type": "Point", 
+        "coordinates": [128.53255,2.48924 ]
+    }]
+}
